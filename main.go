759451e49f47a00c7d97383c925f8f67edac3f35
Michael Street <5597260+MStreet3@users.noreply.github.com>
2022-01-29 18:57:50 -0500

    rename var

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 func twoSum(nums []int32, target int32) ([]int32, bool) {
 
-	diffMap := make(map[int32]int32)
+	complements := make(map[int32]int32)
 
 	for i, n := range nums {
-		diffMap[n] = int32(i)
+		complements[n] = int32(i)
 	}
 
 	for i, n := range nums {
 		complement := target - n
-		if j, prs := diffMap[complement]; prs && j != int32(i) {
+		if j, prs := complements[complement]; prs && j != int32(i) {
 			return []int32{int32(i), int32(j)}, true
 		}
 	}
